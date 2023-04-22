4ce4b1f9ad8f3308fda352ede5f755817e588feb
Dalvis <Dalvis.liz27@gmail.com>
2023-04-21 20:51:01 -0400

    styled App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ import styled from 'styled-components';
 
 const AppWrapper = styled.div`
   display: flex;
-
+  flex-direction: column;
+  header {
+    display: flex;
+    justify-content: center;
+    background-color: #32B8D8;
+    box-shadow: 0 8px 6px -6px rgba(0, 0, 0, 0.5);
+    height: 100px;
+  }
+  h1 {
+    font-size: 3rem;
+    color: white;
+    text-align: center;
+    align-self: center;
+  }
+  h2 {
+    font-size: 2rem;
+    margin: 10px 0;
+  }
+  ol {
+    font-size: 1.2rem;
+  }
 `;
 
 function App(props) {
 
 
   return (
-    <div className="App">
+    <AppWrapper>
       <header>
-        <h1>Nationalize your Name</h1>
+        <h1>Nationalize Your Name</h1>
       </header>
       <div className='wrapper'>
         <div className='instructions'>
@@ -24,13 +44,13 @@ function App(props) {
             <li>Enter your name.</li>
             <li>Click the submit button.</li>
             <li>See the probability of your name being from a certain country.</li>
-            <li>You will receive a country code, this country code corresponds to the ISO country codes found <a href={`https://nationalize.io/our-data`} target='_blank'>here</a>.</li>
+            <li>Note: You will receive a country code, this country code corresponds to the ISO country codes found <a href={`https://nationalize.io/our-data`} target='_blank'>here</a>.</li>
           </ol>
         </div>
       </div>
       <Form />
       {props.nameProbabilities.length > 0 && <Results />}
-    </div>
+    </AppWrapper>
   );
 }
 const mapStateToProps = state => {
